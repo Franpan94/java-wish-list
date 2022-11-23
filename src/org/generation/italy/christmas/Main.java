2afa312d6830c95106a5d76e8f3d63a540902841
Franpan94 <francescopanico94@libero.it>
2022-11-23 14:56:49 +0100

    add comparator

--- src/org/generation/italy/christmas/Main.java
+++ src/org/generation/italy/christmas/Main.java
@@ -2,10 +2,19 @@ package org.generation.italy.christmas;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+	public static class StringComparator implements Comparator <String> {
+
+		@Override
+		public int compare(String o1, String o2) {
+			return 0;
+		}
+		
+	}
      public static void main(String[] args) {
 		List<String> wishList = new ArrayList<>();
 		Scanner sc = new Scanner(System.in);
@@ -21,7 +30,7 @@ public class Main {
         			int listLength = wishList.size();
         			System.out.println("Lunghezza lista: " + listLength);
         		} else if(request.equals("no")) {
-        			Collections.sort(wishList);
+        			wishList.sort(new StringComparator());
         			System.out.println("Lista dei desideri: " + wishList);
         		} else System.out.println("Inserisci una risposta valida");
         			
