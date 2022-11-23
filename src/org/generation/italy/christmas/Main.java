98a00b1dd37b8a8d8b9e41af03682a2573425376
Franpan94 <francescopanico94@libero.it>
2022-11-23 13:10:23 +0100

    finish

--- src/org/generation/italy/christmas/Main.java
+++ src/org/generation/italy/christmas/Main.java
@@ -1,19 +1,16 @@
 package org.generation.italy.christmas;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
      public static void main(String[] args) {
 		List<String> wishList = new ArrayList<>();
-		
-		
 		Scanner sc = new Scanner(System.in);
-		boolean userChoise = true;
-     
         	
-        	while(userChoise) {
+        	while(true) {
         		System.out.print("Per aggiungere un desiderio digitare si, per controllare la lista digitare no: ");
         		String request = sc.nextLine();
         		if(request.equals("si")) {
@@ -21,14 +18,14 @@ public class Main {
             		System.out.print("Aggiungi un desiderio: ");
         			String userRequest = sc.nextLine();
         			wishList.add(userRequest);
-        			System.out.println(wishList);
-        		} else 
-        			System.out.println(wishList);
-        		
+        			int listLength = wishList.size();
+        			System.out.println("Lunghezza lista: " + listLength);
+        		} else if(request.equals("no")) {
+        			Collections.sort(wishList);
+        			System.out.println("Lista dei desideri: " + wishList);
+        		} else System.out.println("Inserisci una risposta valida");
+        			
             }
         
-			
-		
-		
-	}
+		}
 }
